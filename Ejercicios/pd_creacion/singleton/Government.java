744bd66714a990c7cf2fb39f9e0532774d1ddc44
bmgabi95 <98468662+bmgabi95@users.noreply.github.com>
2022-01-26 12:58:51 -0500

    Commit Singleton

---
+++ Ejercicios/pd_creacion/singleton/Government.java
@@ -0,0 +1,17 @@
+package singleton;
+
+public class Government {
+
+    static Government government;
+
+    private Government () { }
+
+    public synchronized static Government getGovernment(){
+        if(government == null){
+            government = new Government();
+        }
+        return government;
+
+    }
+
+}
